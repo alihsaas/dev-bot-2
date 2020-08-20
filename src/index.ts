302d62eccbaba9a910d779b71d0839ac7ee578eb
alihsaas <40891677+alihsaas@users.noreply.github.com>
2020-08-20 16:23:46 +0300

    github-glitch-sync

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,47 @@
 import { Client } from "discord.js";
 import { CommandHandler } from "./commands/command_handler";
-import  express from "express";
-import  keepalive from "express-glitch-keepalive";
-
+import { createHmac } from "crypto";
+import http from "http";
+import express from "express";
+import cmd from "node-cmd";
 
 const client = new Client();
 const commandHandler = new CommandHandler(process.env.PREFIX);
+
 const app = express();
-
-app.use(keepalive);
-
-app.get("/", (req, res) => {
-  res.json("This bot should be online! Uptimerobot will keep it alive");
-});
 app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+app.post('/git', (req, res) => {
+  const hmac = createHmac("sha1", process.env.SECRET);
+  const sig  = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+
+  if (req.headers['x-github-event'] === "push") {
+    if (sig === req.headers['x-hub-signature']) {
+      cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+      cmd.get('./git.sh', (err, data) => {  // Run our script
+        if (data) console.log(data);
+        if (err) console.log(err);
+      });
+      cmd.run('refresh');  // Refresh project
+
+      const commits = req.body.head_commit.message.split("\n").length === 1 ?
+      req.body.head_commit.message :
+      req.body.head_commit.message.split("\n").map((el, i) => i !== 0 ? "                       " + el : el).join("\n");
+      console.log(`> [GIT] Updated with origin/master\n` +
+        `        Latest commit: ${commits}`);
+
+    }
+
+    return res.sendStatus(200); // Send back OK status
+  }
+});
+
 app.listen(process.env.PORT);
 
 
