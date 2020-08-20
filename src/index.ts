7c7611c8890d8205aefe33470540a5d2ed79da49
alihsaas <alihsaas2@gmail.com>
2020-08-20 11:39:26 +0300

    express keep alive

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import { Client } from "discord.js";
 import { CommandHandler } from "./commands/command_handler";
+import  express from "express";
+import  keepalive from "express-glitch-keepalive";
+
 
 const client = new Client();
 const commandHandler = new CommandHandler(process.env.PREFIX);
+const app = express();
+
+app.use(keepalive);
+
+app.get("/", (req, res) => {
+  res.json("This bot should be online! Uptimerobot will keep it alive");
+});
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
 
 client.on(`ready`, () => console.log(`Logged in as ${client.user.tag}`));
 
